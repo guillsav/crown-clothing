39a776d6fb3a9bab9a7a0b7c9b9b9960099c070d
guillsav <guillsav@gmail.com>
2019-07-11 15:12:02 -0400

    Finish styling menu-items

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Hompage from './pages/homepage/homepage.component';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <Hompage />
-    </div>
-  );
+    return (
+        <div>
+            <Hompage />
+        </div>
+    );
 }
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import './menu-item.style.scss'
+import './menu-item.style.scss';
 
-const MenuItem = ({title, id, image, size, link}) => (
-    <div style={{ backgroundImage: `url(${image})` }} className={`${size} menu-item`}>
-    <div className="content">
-        <h2 className="title">{title}</h2>
-        <span className="subtitle">SHOP NOW</span>
+const MenuItem = ({ title, id, image, size, link }) => (
+    <div className={`${size} menu-item`}>
+        <div
+            className="background-image"
+            style={{
+                backgroundImage: `url(${image})`
+            }}
+        />
+        <div className="content">
+            <h2 className="title">{title.toUpperCase()}</h2>
+            <span className="subtitle">SHOP NOW</span>
+        </div>
     </div>
-</div>
 );
 
 export default MenuItem;
